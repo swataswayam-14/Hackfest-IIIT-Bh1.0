cfaa17020fac64910f4f75b75ae60fe5f139f986
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-03-01 03:10:59 +0530

    routes for the components added in app

--- client/MyProject/src/App.jsx
+++ client/MyProject/src/App.jsx
@@ -2,33 +2,47 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import SignUpSide from './components/SignUp'
+import SignInSide from './components/SignIn'
+import CropDetails from './components/CropDetails'
+import BuyerInSide from './components/BuyerSignIn'
+import BuyerSignUp from './components/BuyerSignUp'
+import {BrowserRouter as Router , Route, Routes} from "react-router-dom"
+import UploadCrop from './components/UploadCrop'
+import FarmersList from './components/FarmerList'
+import FarmerDetails from './components/FarmerDetails'
+import DashBoard from './components/DashBoard'
+import FarmerProfile from './components/FarmerProfile'
+import BuyerProfile from './components/BuyerProfile'
+import FarmerCrop from './components/FarmerCrop'
+import EditFarmerAccount from './components/EditFarmerAccount'
+import EditBuyerAccount from './components/EditBuyerProfile'
+import DeleteCurrent from './components/DeleteCurrent'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<DashBoard/>}/>
+        <Route path="/farmersignin" element={<SignInSide/>}/>
+        <Route path="/farmersignup" element={<SignUpSide/>}/>
+        <Route path='/buyersignin' element={<BuyerInSide/>}/>
+        <Route path='/buyersignup' element={<BuyerSignUp/>}/>
+        <Route path='/cropdetails/:id' element={<CropDetails/>}/>
+        <Route path='/uploadCrop/:id' element= {<UploadCrop/>}/>
+        <Route path='/farmers' element = {<FarmersList/>}/>
+        <Route path='/farmers/:id' element={<FarmerDetails/>}/>
+        <Route path='/profile/:id' element={<FarmerProfile/>}/>
+        <Route path='/buyer/:id' element={<BuyerProfile/>}/>
+        <Route path='/farmercrop/:id' element={<FarmerCrop/>}/>
+        <Route path='/editaccount/:id' element={<EditFarmerAccount/>}/>
+        <Route path='/editbuyer/:id' element = {<EditBuyerAccount/>}/>
+        <Route path='/deletecurrent/:id' element={<DeleteCurrent/>}/>
+      </Routes>
+    </Router>
   )
 }
 
